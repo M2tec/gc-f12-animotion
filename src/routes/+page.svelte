01217cdcfe9a26ec51bed824c758d970ff1b6412
Maarten Menheere <m.menheere@m2tec.nl>
2024-11-28 12:03:46 -0300

    test static

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,23 +15,21 @@
 	let code5: Code
 	let code6: Code
 	let code7: Code
-	import Playground_1 from '$lib/assets/workshop_1.png'
-	// import DSL_1 from '$lib/assets/workshop_3.png'
 </script>
 
 <Presentation options={{ history: true, transition: 'slide', controls: false, progress: false }}>
 	<Slide class="h-full place-content-center place-items-center">
-		<img bind:this={image} style="height: 900px;" src={Playground_1} alt="GC workshop" />
+		<img bind:this={image} style="height: 900px;" src=/assets/workshop_1.png alt="GC workshop" />
 	</Slide>
  
 	<Slide class="h-full place-content-center place-items-center">
-		<img style="height: 900px;" src='$lib/assets/workshop_1.png' alt="Welcome" />
+		<img style="height: 900px;" src='/assets/workshop_1.png' alt="Welcome" />
 
 	</Slide>
 
 	<Slide class="h-full place-content-center place-items-center">
 		<video width="1920" height="1080" autoplay controls>
-			<track default kind="captions" srclang="en" src="$lib/assets/GC_Intro_Edu_Gray.mp4" />
+			<track default kind="captions" srclang="en" src="/assets/GC_Intro_Edu_Gray.mp4" />
 			<source src="$lib/assets/GC_Intro_Edu_Gray.mp4" type="video/mp4" />
 			Your browser does not support the video tag.
 		</video>
