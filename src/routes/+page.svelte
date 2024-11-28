5f3c4cf4cee6abbb3c075d41e2f3b09c25a6b557
Maarten Menheere <m.menheere@m2tec.nl>
2024-11-28 12:07:06 -0300

    test static2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,14 +23,14 @@
 	</Slide>
  
 	<Slide class="h-full place-content-center place-items-center">
-		<img style="height: 900px;" src='/assets/workshop_1.png' alt="Welcome" />
+		<img style="height: 900px;" src='/assets/workshop_2.png' alt="Welcome" />
 
 	</Slide>
 
 	<Slide class="h-full place-content-center place-items-center">
 		<video width="1920" height="1080" autoplay controls>
 			<track default kind="captions" srclang="en" src="/assets/GC_Intro_Edu_Gray.mp4" />
-			<source src="$lib/assets/GC_Intro_Edu_Gray.mp4" type="video/mp4" />
+			<source src="/assets/GC_Intro_Edu_Gray.mp4" type="video/mp4" />
 			Your browser does not support the video tag.
 		</video>
 	</Slide>
